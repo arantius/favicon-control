133433a79fb9ef1970675654ffd499c42640781d
Anthony Lieuallen <alieuall@google.com>
2013-11-29 16:36:27 -0500

    Basic display of current icon works.

--- content/overlay.js
+++ content/overlay.js
@@ -1,9 +1,15 @@
 (function() {
+  Components.utils.import("resource://gre/modules/PlacesUtils.jsm");
+
   var _origInitPanel = gEditItemOverlay.initPanel;
   gEditItemOverlay.initPanel = function(aFor, aInfo) {
     _origInitPanel.call(gEditItemOverlay, aFor, aInfo);
 
-  document.getElementById('favicon-control-img')
-      .setAttribute('src', 'moz-anno:favicon:' + gEditItemOverlay.uri.spec);
+  PlacesUtils.favicons.getFaviconURLForPage(
+      gEditItemOverlay.uri,
+      function(aUri) {
+      document.getElementById('favicon-control-img')
+          .setAttribute('src', 'moz-anno:favicon:' + aUri.spec);
+      });
   };
 })();
